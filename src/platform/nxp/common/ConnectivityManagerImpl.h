072b01e972f005f416698ea81fc52447f95dd5a5
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2025-03-10 17:46:07 +0100

    Revert "[nxp toup][border router][common] Update to new br rtos manager interface"
    This reverts commit e4f505f5a26a9395dd82875d15548ee05204c506.
    
    Signed-off-by: Dina Benamar <dina.benamarelmaaroufi@nxp.com>

--- src/platform/nxp/common/ConnectivityManagerImpl.h
+++ src/platform/nxp/common/ConnectivityManagerImpl.h
@@ -163,8 +163,6 @@ private:
     void UpdateInternetConnectivityState(void);
 #if CHIP_ENABLE_OPENTHREAD
     void BrHandleStateChange();
-    static void LockThreadStack();
-    static void UnlockThreadStack();
 #endif /* CHIP_DEVICE_CONFIG_ENABLE_THREAD */
 #endif
     /* CHIP_DEVICE_CONFIG_ENABLE_WPA */
