e9fc0342e218eae28238faeff636e74dfd21e278
maximus <maximus.sun@nxp.com>
2025-06-10 17:17:37 +0800

    Add hardware security supporting(Trusty OS, ele)
    Provides the driver code for Trusty TEE and ELE.
    Implements a portion of hardware-based security APIs.
    
    Below patches are picked:
        * MA-21007 integrate libtrustymatter
        * MA-21007-2 support device attestation based on Trusty OS
        * MA-21007-3 support P256KeyPair based on Trusty OS
        * MA-21129 support trusty backed operational keystore
        * MATTER-1352 support ele backed operational keystore
        * MATTER-1672-1: add device attestation based on ELE
        * MATTER-1934 fix the build break after codebase upgrade
        * MATTER-2081-1 fix matter build break after codebase upgrade
        * MATTER-2389-2 fix the build after syncing imx-secure-enclave
        * MATTER-2900-2 Skip fabric id deletion if it doesn't exist
    
    Change-Id: Ifc797dca41c94f5064745dc98e49d6e40b191ab5
    Signed-off-by: maximus <maximus.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/29243
    Reviewed-by: Elven Wang <elven.wang@nxp.com>

--- scripts/build/build/targets.py
+++ scripts/build/build/targets.py
@@ -752,6 +752,7 @@ def BuildIMXTarget():
 
     target.AppendModifier('release', release=True)
     target.AppendModifier('trusty', trusty=True)
+    target.AppendModifier('ele', ele=True)
 
     return target
 
