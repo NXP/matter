e3453c17565f8899b47d99d808ad282169efb38a
Haoran Wang <elven.wang@nxp.com>
2025-07-11 11:00:00 +0800

    MATTER-3470-4 enable nxp-media-app
    This patch picked all changes applied to nxp-media-app.
    
    Below patches are picked:
        * MATTER-1805-4 Fix nxp-media app sink not found in imx91
        * MATTER-2796 nxp-media-app: it can't play music successfully after pause action. 100%
        * MATTER-2869-3 Update nxp-media-common to v1.4
        * MATTER-2484 Add delay to IPC
        * MATTER-2259-2 Fixed build error for nxp reference demos
        * MATTER-2074-2 Align with TvApp for nxp-media-app
        * MATTER-1802 only trigger the play command when mediaplayback play set
        * MATTER-1928 Upgrade the nxp-media-app
        * MATTER-1805-3 Added the jsoncpp dependency for nxp-media-common
        * MATTER-1805-2 support pipewire in nxp-media-app
        * MATTER-1805 Fix nxp-media-app build error
        * MATTER-1573-8 Fixed the communication issue of nxp-media-app
        * MATTER-1573-7 Add audio output select function
        * MATTER-1573-6 Fix some bugs for nxp-media-app
        * MATTER-1573-4 Check the gplay status before trigger the commands
        * MATTER-1573-3 Add Application Launcher support
        * MATTER-1573-2 Connect nxp-media-app to imx gplay
        * MATTER-1573 Initial version of nxp-media-app
    
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>
    Signed-off-by: Junmeng Li <junmeng.li_1@nxp.com>
    Signed-off-by: nxg07908 <tommy.chen@nxp.com>
    Change-Id: I8fa65c4a1643ed601805d5036b68908b31e2a680
    Reviewed-on: http://androidsource.nxp.com/project/29228

---
+++ examples/nxp-media-app/nxp-media-common/src/AudioOutput.cpp
@@ -0,0 +1,76 @@
+#include "AudioOutput.h"
+
+#include <fstream>
+#include <iostream>
+#include <json/json.h>
+#include <lib/core/CHIPCore.h>
+#include <string>
+
+int AudioOutput::Init() {
+    int ret = system("systemctl --user status pipewire wireplumber pipewire-pulse > /dev/null");
+    if (ret != 0) {
+        int retry = 3;
+        while (retry--) {
+            ChipLogError(NotSpecified, "pipewire, wireplumber and pipewire-pulse not started. Enabling...");
+            ret = system("systemctl --user start pipewire wireplumber pipewire-pulse && sleep 5");
+            if (ret != 0) {
+                ChipLogError(NotSpecified, "Failed to enable pipewire ,wireplumber and pipewire-pulse.");
+            }
+            ret = system("systemctl --user status pipewire wireplumber pipewire-pulse > /dev/null");
+            if (ret != 0) {
+                ChipLogError(NotSpecified, "manual enabling pipewire failed...");
+            } else {
+                break;
+            }
+        }
+        if (ret != 0) {
+            ChipLogError(NotSpecified, "With multiple retry, failed to kick pipewire, wireplumber and pipewire-pulse, return");
+            return ret;
+        }
+    }
+    std::string cmd = "pw-dump > /tmp/pw_dump.json";
+    ret = system(cmd.c_str());
+    if (ret != 0) {
+        ChipLogError(NotSpecified, "Failed to get pw dump.");
+        return ret;
+    }
+
+    //start to parse json
+    std::ifstream input("/tmp/pw_dump.json");
+    Json::Value root;
+    Json::Reader reader;
+    bool parsedSuccess = reader.parse(input, root);
+
+    if (!parsedSuccess) {
+        ChipLogError(NotSpecified, "Failed to parse JSON");
+        return 1;
+    }
+
+    for (unsigned int i = 0; i < root.size(); i++) {
+        Json::Value item = root[i];
+        int id = item["id"].asInt();
+        if (item["info"].isMember("props")) {
+            if (item["info"]["props"].isMember("factory.name")) {
+                if (item["info"]["props"]["factory.name"].asString().find("sink") != std::string::npos) {
+                    std::string cardName = item["info"]["props"]["alsa.long_card_name"].asString();
+                    sink_list.push_back(cardName);
+                    sinks_[cardName] = id;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+int AudioOutput::Select(uint32_t number) {
+    if(number >= sinks_.size()) {
+        ChipLogError(NotSpecified, "Invalid sink number");
+        return -1;
+    }
+
+    std::string cmd = "wpctl set-default " + std::to_string(sinks_[sink_list[number]]);
+
+    return system(cmd.c_str());
+
+}
