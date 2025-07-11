2073916e3e9413c497c25cf2c16d255d96930ecd
Haoran Wang <elven.wang@nxp.com>
2025-07-11 10:55:23 +0800

    MATTER-3470-2 matter certification v1.4 modification changes
    This patch picked all matter certification v1.4 modification changes for
    below clusters:
       * nxp-water-heater-app
       * nxp-heat-pump-app
       * nxp-network-manager-app
       * nxp-device-energy-management-app
       * nxp-evse-app
       * nxp-battery-storage-app
       * nxp-solar-power-app
    
    Below patches are picked:
        * Add the example, nxp-device-energy-management-app, for Matter 1.4 certification
        * Add the example, nxp-water-heater-app, for Matter 1.4 certification
        * Add the example, nxp-solar-power-app, for Matter 1.4 certification
        * Add the example, nxp-evse-app, for Matter 1.4 certification
        * Add the example, nxp-heat-pump-app, for Matter 1.4 certification
        * Add the example, nxp-battery-storage, for Matter 1.4 certification
    
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>
    Signed-off-by: Lo,Chin-Ran <chin-ran.lo@nxp.com>
    Signed-off-by: Jason Lan <zhen-yu.lan@nxp.com>
    Change-Id: Ia102f7dfddf9f8347ad2b8a0a395bc4c14880ebe
    Reviewed-on: http://androidsource.nxp.com/project/29226
    Reviewed-by: Junmeng Li <junmeng.li_1@nxp.com>

---
+++ examples/nxp-water-heater-app/nxp-water-heater-common/water-heater/src/WaterHeaterMain.cpp
@@ -0,0 +1,113 @@
+/*
+ *
+ *    Copyright (c) 2024 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "EnergyManagementAppCmdLineOptions.h"
+
+#include <DeviceEnergyManagementDelegateImpl.h>
+#include <DeviceEnergyManagementManager.h>
+#include <ElectricalPowerMeasurementDelegate.h>
+#include <PowerTopologyDelegate.h>
+#include <WhmManufacturer.h>
+#include <device-energy-management-modes.h>
+#include <water-heater-mode.h>
+
+#include <app-common/zap-generated/ids/Attributes.h>
+#include <app-common/zap-generated/ids/Clusters.h>
+#include <app/ConcreteAttributePath.h>
+#include <app/clusters/electrical-energy-measurement-server/electrical-energy-measurement-server.h>
+#include <app/clusters/network-commissioning/network-commissioning.h>
+#include <app/clusters/power-topology-server/power-topology-server.h>
+#include <app/server/Server.h>
+#include <lib/support/logging/CHIPLogging.h>
+#include <platform/Linux/NetworkCommissioningDriver.h>
+
+#include <DEMDelegate.h>
+#include <EnergyEvseMain.h>
+#include <WhmMain.h>
+
+#include <app-common/zap-generated/attributes/Accessors.h>
+using namespace chip::app::Clusters::DeviceEnergyManagement;
+using namespace chip::app::Clusters::ElectricalPowerMeasurement;
+extern ElectricalPowerMeasurementDelegate * GetEPMDelegate();
+
+namespace chip {
+namespace app {
+namespace Clusters {
+namespace WaterHeaterManagement {
+
+void FullWhmApplicationInit(bool DoEnergyInit)
+{
+    ReturnOnFailure(WhmApplicationInit());
+
+    if (DoEnergyInit == true)
+    {
+        if (DeviceEnergyManagementInit() != CHIP_NO_ERROR)
+        {
+            WhmApplicationShutdown();
+            return;
+        }
+/*
+        if (EnergyMeterInit() != CHIP_NO_ERROR)
+        {
+            DeviceEnergyManagementShutdown();
+            WhmApplicationShutdown();
+            return;
+        }
+
+        if (PowerTopologyInit() != CHIP_NO_ERROR)
+        {
+            EnergyMeterShutdown();
+            DeviceEnergyManagementShutdown();
+            WhmApplicationShutdown();
+            return;
+        }
+*/
+    }
+    /* For Device Energy Management we need the ESA to be Online and ready to accept commands */
+
+    GetDEMDelegate()->SetESAState(ESAStateEnum::kOnline);
+    GetDEMDelegate()->SetESAType(ESATypeEnum::kWaterHeating);
+    GetDEMDelegate()->SetDEMManufacturerDelegate(*GetWhmManufacturer());
+
+    // Set the abs min and max power
+    GetDEMDelegate()->SetAbsMinPower(1200000); // 1.2KW
+    GetDEMDelegate()->SetAbsMaxPower(7600000); // 7.6KW
+
+    // Init PowerMode = 2 for TC-EPM-2.1
+    ChipLogDetail(AppServer, "Init to set PowerModeEnum::kAc");
+    //GetEPMDelegate()->SetPowerMode(PowerModeEnum::kAc);
+}
+
+void FullWhmApplicationShutdown()
+{
+    ChipLogDetail(AppServer, "Energy Management App (WaterHeater): ApplicationShutdown()");
+
+    /* Shutdown in reverse order that they were created */
+    PowerTopologyShutdown();          /* Free the PowerTopology */
+    EnergyMeterShutdown();            /* Free the Energy Meter */
+    DeviceEnergyManagementShutdown(); /* Free the DEM */
+    WhmApplicationShutdown();
+
+    Clusters::DeviceEnergyManagementMode::Shutdown();
+    Clusters::WaterHeaterMode::Shutdown();
+}
+
+} // namespace WaterHeaterManagement
+} // namespace Clusters
+} // namespace app
+} // namespace chip
