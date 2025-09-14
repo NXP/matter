f3e9fd1fd2c4883b60b4deb99a3d1c6c3d1934bc
Gatien Chapon <gatien.chapon@nxp.com>
2025-09-14 18:03:30 +0200

    Revert "[nxp fromtree] [NXP][platform][common] Fix wifi commissioning issue due to none register cluster (#39764)"
    This reverts commit 80dd893423aaa5c02e1c6e1af1f752604b3e6292.

--- src/platform/nxp/common/NetworkCommissioningWiFiDriver.cpp
+++ src/platform/nxp/common/NetworkCommissioningWiFiDriver.cpp
@@ -69,13 +69,7 @@ CHIP_ERROR NXPWiFiDriver::Init(NetworkStatusChangeCallback * networkStatusChange
     if (err != CHIP_NO_ERROR)
     {
         ChipLogProgress(DeviceLayer, "WiFi network SSID not retrieved from persisted storage: %" CHIP_ERROR_FORMAT, err.Format());
-        if (err != CHIP_ERROR_PERSISTED_STORAGE_VALUE_NOT_FOUND)
-        {
-            return err;
-        }
-        /* AP ssid has not been set. It's possible after factory-reset.
-        Do not return an error as it will cancel the cluster registration */
-        return CHIP_NO_ERROR;
+        return err;
     }
 
     err = PersistedStorage::KeyValueStoreMgr().Get(kWiFiCredentialsKeyName, mSavedNetwork.credentials,
