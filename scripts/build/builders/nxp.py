bcf82b4341a6c25eee814bcac0974d35b32d2a84
Gatien Chapon <gatien.chapon@nxp.com>
2025-12-08 16:25:50 +0100

    Revert "[nxp fromtree][NXP][platform][mcxw72] Add CMake build support (#41039)"
    This reverts commit 45fab9feb95d3e13d69ed18b6709834f6cec0696.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- scripts/build/builders/nxp.py
+++ scripts/build/builders/nxp.py
@@ -388,11 +388,8 @@ class NxpBuilder(GnBuilder):
         if self.se05x_enable:
             flags.append('-DCONFIG_CHIP_SE05X=y')
 
-        if self.board in (NxpBoard.MCXW71, NxpBoard.MCXW72):
+        if self.board == NxpBoard.MCXW71:
             flags.append('-DCONFIG_MCUX_COMPONENT_middleware.freertos-kernel.config=n')
-
-        if self.board == NxpBoard.MCXW72:
-            flags.append('-Dcore_id=cm33_core0')
 
         build_flags = " ".join(flags)
 
