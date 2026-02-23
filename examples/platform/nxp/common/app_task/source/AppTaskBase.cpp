76e0476285703e6cb01600f20ea5d60eeb595e80
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2026-02-23 11:29:50 +0100

    Revert "[nxp fromtree] [NXP][platform][common] Fix DeviceInfoProvider initialization order regression in Matter server startup (#42998)"
    This reverts commit 53de8cbbc5e57ead7288f331c4042628d81b1f6a.
    
    Signed-off-by: Dina Benamar <dina.benamarelmaaroufi@nxp.com>

--- examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
+++ examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
@@ -189,12 +189,6 @@ void chip::NXP::App::AppTaskBase::InitServer(intptr_t arg)
     initParams.operationalKeystore = chip::NXP::App::OperationalKeystore::GetInstance();
 #endif
     (void) initParams.InitializeStaticResourcesBeforeServerInit();
-
-#if CONFIG_DEVICE_INFO_PROVIDER_IMPL
-    gExampleDeviceInfoProvider.SetStorageDelegate(initParams.persistentStorageDelegate);
-    chip::DeviceLayer::SetDeviceInfoProvider(&gExampleDeviceInfoProvider);
-#endif
-
     initParams.dataModelProvider = app::CodegenDataModelProviderInstance(initParams.persistentStorageDelegate);
 
 #if CONFIG_NET_L2_OPENTHREAD
@@ -207,10 +201,14 @@ void chip::NXP::App::AppTaskBase::InitServer(intptr_t arg)
 #endif
 
     VerifyOrDie((chip::Server::GetInstance().Init(initParams)) == CHIP_NO_ERROR);
-
-#if CONFIG_CHIP_APP_OPERATIONAL_KEYSTORE
     auto * persistentStorage = &Server::GetInstance().GetPersistentStorage();
+#if CONFIG_CHIP_APP_OPERATIONAL_KEYSTORE
     TEMPORARY_RETURN_IGNORED chip::NXP::App::OperationalKeystore::Init(persistentStorage);
+#endif
+
+#if CONFIG_DEVICE_INFO_PROVIDER_IMPL
+    gExampleDeviceInfoProvider.SetStorageDelegate(persistentStorage);
+    chip::DeviceLayer::SetDeviceInfoProvider(&gExampleDeviceInfoProvider);
 #endif
 
     GetAppTask().PostInitMatterServerInstance();
