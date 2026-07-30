5ba867bb591f4106101cad49dce96fda1de8a567
Cristian Bulacu <127317648+Cristib05@users.noreply.github.com>
2026-07-30 15:39:22 +0300

    [nxp fromtree] [NXP] Set LwIP default interface based on Thread/Wi-Fi state (Auto-merged by platform-bot)
    While the border router is not yet fully up (Wi-Fi not connected) and
    the Thread link is up, the Thread netif is used as the default so that
    outbound connectivity is available over Thread during bring-up.
    
    Once the border router is fully initialised (both Wi-Fi and Thread
    running), the explicit Thread default is cleared so LwIP selects the
    infrastructure interface, matching the border-router routing model.
    
    The selection is refreshed on the events that can change interface state
    (Thread state changes and IPv6 connectivity changes) and the netif update
    is performed under LOCK_TCPIP_CORE()/UNLOCK_TCPIP_CORE() to stay atomic
    with respect to the TCP/IP core.
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>
    (cherry picked from commit ff334ff)

--- src/platform/nxp/common/ConnectivityManagerImpl.h
+++ src/platform/nxp/common/ConnectivityManagerImpl.h
@@ -156,6 +156,10 @@ private:
 #endif // CHIP_DEVICE_CONFIG_NXP_WAIT_FOR_ROUTABLE_IPV6
 #endif
 
+#if (CHIP_DEVICE_CONFIG_ENABLE_WPA || CONFIG_CHIP_ETHERNET) && CHIP_DEVICE_CONFIG_ENABLE_THREAD
+    void UpdateLwipDefaultNetIf();
+#endif
+
 #if CHIP_DEVICE_CONFIG_ENABLE_WPA
     ConnectivityManager::WiFiStationMode mWiFiStationMode;
     ConnectivityManager::WiFiStationState mWiFiStationState = kWiFiStationState_NotConnected;
